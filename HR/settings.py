e5df1738b7de8a62bd5eff27be20dbcddb7d598d
MihaiElisei <mihai.m.elisei@gmail.com>
2023-02-03 01:19:50 +0000

    fix allowed hosts

--- HR/settings.py
+++ HR/settings.py
@@ -28,15 +28,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-
+ALLOWED_HOSTS = ["localhost"]
 # Add Render.com URL to allowed hosts
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
-ALLOWED_HOSTS = ["localhost"]
+
 
 
 # Application definition

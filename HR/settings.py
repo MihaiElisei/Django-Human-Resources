a892548521c68f8e9f273ab85a519d1c006d78ab
MihaiElisei <mihai.m.elisei@gmail.com>
2023-02-03 01:12:07 +0000

    deployment for render

--- HR/settings.py
+++ HR/settings.py
@@ -30,6 +30,12 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+
+# Add Render.com URL to allowed hosts
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 ALLOWED_HOSTS = ["localhost"]
 
 
